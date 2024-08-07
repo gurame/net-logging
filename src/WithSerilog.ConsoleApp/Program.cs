4513471ac6d5afbdd0384f91fd014152547a7486
Gustavo Rabanal <15724234+gurame@users.noreply.github.com>
2024-08-07 15:33:24 -0500

    feat: add serilog timing

--- src/WithSerilog.ConsoleApp/Program.cs
+++ src/WithSerilog.ConsoleApp/Program.cs
@@ -3,10 +3,11 @@ using Serilog;
 using Serilog.Context;
 using Serilog.Formatting.Json;
 using Serilog.Sinks.SystemConsole.Themes;
+using SerilogTimings.Extensions;
 
 ILogger logger = new LoggerConfiguration()
-	//.WriteTo.Console(theme: AnsiConsoleTheme.Code)
-	.WriteTo.Console(new JsonFormatter())
+	.WriteTo.Console(theme: AnsiConsoleTheme.Code)
+	//.WriteTo.Console(new JsonFormatter())
 	.Enrich.FromLogContext()
 	.Destructure.ByTransforming<Payment>(p => new { p.PaymentId, p.UserId })
 	.CreateLogger();
@@ -32,6 +33,12 @@ logger.Information("Payment processed {@Payment}", payment);
 using(LogContext.PushProperty("PaymentId", payment.PaymentId))
 {
 	logger.Information("User {UserId} processed payment", payment.UserId);
+}
+
+using(logger.TimeOperation("Processing payment {PaymentId}", payment.PaymentId))
+{
+	Thread.Sleep(1000);
+	logger.Information("Received payment by user with id {UserId}", payment.UserId);
 }
 
 Log.CloseAndFlush();
