25e2555abc134f6e3290e242d18b3b066486a4c5
Gustavo Rabanal <15724234+gurame@users.noreply.github.com>
2024-08-07 12:14:39 -0500

    feat: add log scope

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -1,20 +1,21 @@
 ﻿
+using System.Text.Json;
 using ConsoleApp;
 using Microsoft.Extensions.Logging;
 
 using var loggerFactory = LoggerFactory.Create(builder=>{
 	builder.SetMinimumLevel(LogLevel.Debug);
 	builder.AddJsonConsole(x=>{
-		x.IncludeScopes = false;
+		x.IncludeScopes = true;
 		x.TimestampFormat = "HH:mm:ss";
-		x.JsonWriterOptions = new System.Text.Json.JsonWriterOptions
+		x.JsonWriterOptions = new JsonWriterOptions
 		{
 			Indented = true
 		};
 	});
 
-	builder.ClearProviders();
-	builder.AddSimpleConsole();
+	//builder.ClearProviders();
+	//builder.AddSimpleConsole();
 });
 
 ILogger logger = loggerFactory.CreateLogger<Program>();
@@ -33,3 +34,43 @@ catch (Exception ex)
 {
 	logger.LogError(ex, "Hello from Error, {Name}! You are {Age} years old.", name, age);
 }
+
+// customize templates
+
+var paymentId = 1;
+var amount = 17.99;
+var date = DateTime.Now;
+
+logger.LogInformation("Payment {PaymentId} for {Amount:C} has been processed at {date:F}.", paymentId, amount, date);
+
+
+// Scopes
+using (logger.BeginScope("Payment {PaymentId}", paymentId))
+{
+	try
+	{
+		logger.LogInformation("Processing payment with amount {Amount:C} at {Date}.", amount, date);
+	}
+	finally
+	{
+		logger.LogInformation("Payment {PaymentId} processed.", paymentId);	
+	}
+}
+
+/*
+var paymentData = new PaymentData
+{
+	PaymentId = 2,
+	Amount = 19.99m,
+	Date = DateTime.Now
+};
+
+logger.LogInformation("Payment with data: {paymentData}.", paymentData);
+
+class PaymentData
+{
+	public int PaymentId { get; set; }
+	public decimal Amount { get; set; }
+	public DateTime Date { get; set; }
+}
+*/
